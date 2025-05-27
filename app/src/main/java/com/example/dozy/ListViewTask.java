186c2b898abed5b55d205068b5f9b0140128f160
JoelNavarroV <juannavarro@wposs.com>
2025-05-27 08:53:37 -0500

    0005_STRINGS_COLORS
    - Add Strings necesary.
    - Deleted code unused.
    - Add new color in the file.

--- app/src/main/java/com/example/dozy/ListViewTask.java
+++ app/src/main/java/com/example/dozy/ListViewTask.java
@@ -5,38 +5,23 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
-import com.google.android.material.datepicker.MaterialDatePicker;
-
-import java.util.Calendar;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ListViewTask#newInstance} factory method to
  * create an instance of this fragment.
  */
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
-import com.google.android.material.datepicker.MaterialDatePicker;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
@@ -56,18 +41,15 @@ public class ListViewTask extends Fragment {
         Button btnAddDate = view.findViewById(R.id.btnTest);
 
 
-        btnAddDate.setOnClickListener(v -> mostrarDialogoConVistaPersonalizada());
+        btnAddDate.setOnClickListener(v -> showDialogAddTask());
 
         return view;
     }
 
-    private void mostrarDialogoConVistaPersonalizada() {
-        // Inflar el layout del diálogo
+    private void showDialogAddTask() {
+
         LayoutInflater inflater = LayoutInflater.from(requireContext());
         View vista = inflater.inflate(R.layout.dialog_add_task, null);
-
-        // Referencias a vistas internas del diálogo
-
 
         AlertDialog dialog = new AlertDialog.Builder(requireContext())
                 .setView(vista)
